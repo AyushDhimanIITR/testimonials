cfe27b8f22b0a0520015ee9d5643e021d8abc327
AyushDhimanIITR <ayushd175@gmail.com>
2023-02-01 00:53:46 +0530

    add

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TestiMonials from './Testimonials/Testimonials';
+
+
+function App() {
+  return (
+    <div className="App">
+     <TestiMonials />
+    </div>
+  );
+}
+
+export default App;
